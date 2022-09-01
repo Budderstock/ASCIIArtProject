e7dddfa5afa1907bb45395937283259f6a5ae25e
Budderstock <shanejanish@gmail.com>
2022-09-01 09:35:14 -0500

    Initial commit

---
+++ src/MyAsciiProject.java
@@ -0,0 +1,9 @@
+public class MyAsciiProject {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("             ,\n" +
+                "           __)\\_  \n" +
+                "     (\\_.-'    a`-.\n" +
+                "      (/~~````(/~^^`" );
+    }
+}
